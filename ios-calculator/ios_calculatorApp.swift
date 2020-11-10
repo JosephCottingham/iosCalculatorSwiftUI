9b73092d3859d932e878a1c745507840b45f854e
Joseph Cottingham <joseph.h.cottingham@gmail.com>
2020-11-09 19:34:43 -0500

    Initial Commit

---
+++ ios-calculator/ios_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_calculatorApp.swift
+//  ios-calculator
+//
+//  Created by Joseph Cottingham on 11/9/20.
+//
+
+import SwiftUI
+
+@main
+struct ios_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
